2d1a54886b297673a6b2b9cc83a2a9b52a7fd104
LydiaMamoade <malyf2002@gmail.com>
2018-01-19 15:58:47 +0100

    html5 19/01 16:00

---
+++ HTML5/WebWorker/scripts/monWorker.js
@@ -0,0 +1,20 @@
+﻿var duree = 0, pas = 0, enPause = false, compteur = 0;
+onmessage = function (eventData) {
+    var command = eventData.data.command;
+    switch (command) {
+        case 'init':
+            duree = eventData.data.valeurs.duree;
+            pas = eventData.data.valeurs.pas;
+            break;
+        case 'pause':
+            enPause = !enPause;
+            compte();
+            break;
+    }
+}
+function compte() {
+    compteur += pas;
+    if (!enPause) setTimeout('compte()', duree);
+    postMessage(compteur);
+}
+compte();
